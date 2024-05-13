317d2e16e5759adfc7e6a6d59dcb63c00602a120
gengyoubo <gengyoubo@gmail.com>
2024-05-13 22:29:19 +0900

    Add files via upload

--- src/main/java/github/com/gengyoubo/changedplus/item/Latex_sword.java
+++ src/main/java/github/com/gengyoubo/changedplus/item/Latex_sword.java
@@ -9,7 +9,7 @@ public class Latex_sword extends SwordItem {
 
     // 武器的属性
     public Latex_sword() {
-        super(Tiers.IRON, 12, 6, new Item.Properties().stacksTo(1).tab(ChangedPlusTab.createChangedPlusTab()).durability(1000)); // 设置材质等级为铁，攻击力为12，攻击速度为-2.4，耐久度为1000
+        super(Tiers.IRON, 12, 6, new Item.Properties().stacksTo(1).tab(ChangedPlusTab.getInstance()).durability(1000)); // 设置材质等级为铁，攻击力为12，攻击速度为-2.4，耐久度为1000
     }
 }
 
--- src/main/java/github/com/gengyoubo/changedplus/item/ModEventSubscriber.java
+++ src/main/java/github/com/gengyoubo/changedplus/item/ModEventSubscriber.java
@@ -8,15 +8,19 @@ import net.minecraftforge.fml.common.Mod;
 // 物品注册表
 @Mod.EventBusSubscriber(modid = "changedplus", bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventSubscriber {
-
+    // 注册物品
     @SubscribeEvent
     public static void onRegisterItems(final RegistryEvent.Register<Item> event) {
         registerItem(event, new Latex_sword(), "latex_sword");
-        // 注册更多物品，如果有的话
+        registerItem(event, new Latex(), "latex");
+        registerItem(event, new latex_steak(), "latex_steak");
+        registerItem(event, new long_latex_steak(), "long_latex_steak");
+        // 以registerItem的格式注册物品↑
     }
-    // 注册单个物品
+    //一键注册物品的属性
     private static void registerItem(RegistryEvent.Register<Item> event, Item item, String name) {
         item.setRegistryName("changedplus", name);
         event.getRegistry().register(item);
     }
 }
+
